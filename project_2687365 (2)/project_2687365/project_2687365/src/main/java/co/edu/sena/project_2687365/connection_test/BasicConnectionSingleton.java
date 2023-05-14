789e87412b1a537377c8e8ce82fd0530404bd1b4
Maria-Fernanda-Lozano <128444859+Maria-Fernanda-Lozano@users.noreply.github.com>
2023-05-13 21:54:00 -0500

    Update BasicConnectionSingleton.java

--- project_2687365 (2)/project_2687365/project_2687365/src/main/java/co/edu/sena/project_2687365/connection_test/BasicConnectionSingleton.java
+++ project_2687365 (2)/project_2687365/project_2687365/src/main/java/co/edu/sena/project_2687365/connection_test/BasicConnectionSingleton.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class BasicConnectionSingleton {
     private static String url =
-            "jdbc:mysql://localhost:3307/Juanito?serverTimezone=America/Bogota";
+            "jdbc:mysql://localhost:3307/mafe?serverTimezone=America/Bogota";
     private static String user = "root";
     private static String pass = "";
     private static Connection conn;
